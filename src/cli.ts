6f5a7ca330f4773f439488c5ea47cf2eef598953
Olawoyin Daniel <olawoyindaniel95@gmail.com>
2024-11-25 01:09:50 -0800

    did some minor changes to fix the module not found

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { Command } from "commander";
-import { generateProject, generateComponent, generatePage } from "./generators";
+import { generateProject, generateComponent, generatePage } from "./generators.js";
 
 
 const program = new Command();
