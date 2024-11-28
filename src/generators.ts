275e1475db363dff8c4339307d6f77eec1b3a667
Olawoyin Daniel <olawoyindaniel95@gmail.com>
2024-11-28 02:23:09 -0800

    revert the jest-config

--- src/generators.ts
+++ src/generators.ts
@@ -150,26 +150,9 @@ export const generateProject = async (projectName: string) =>
 
     // Create jest.setup.js if it doesn't exist
     const jestSetupContent = `
-import type { Config } from 'jest'
-import nextJest from 'next/jest.js'
-
-const createJestConfig = nextJest({
-
-  dir: './',
-})
-
-// Add any custom config to be passed to Jest
-const config: Config = {
-  coverageProvider: 'v8',
-  testEnvironment: 'jsdom',
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-}
-
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config)';
+import '@testing-library/jest-dom';
     `;
-    await fs.writeFile('jest.config.ts', jestSetupContent);
+    await fs.writeFile('jest.setup.js', jestSetupContent);
 
     // Install Husky
     mainSpinner.text = "Installing Husky and commitlint...";
